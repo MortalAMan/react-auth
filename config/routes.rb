360a0b02023b20551987d919ecdfca3527686532
Addy <Ashepherdking@gmail.com>
2017-06-29 02:24:29 -0600

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
+
   namespace :api do
-    #API ROUTES SHOULD GO HERE
+    get 'bio', to: 'bio#show'
+    put 'bio', to: 'bio#update'
   end
 
   #Do not place any routes below this one
